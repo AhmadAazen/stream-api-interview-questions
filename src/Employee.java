a806c7b7abcc9b74d0abf0131bfdf9d136743aa8
Aazen Ahmad <ahmadaazen27@gmail.com>
2025-12-07 21:35:44 +0530

    Started interview questions

---
+++ src/Employee.java
@@ -0,0 +1,66 @@
+public class Employee {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private Double salary;
+    private String department;
+
+    public Employee(Integer id, String name, Integer age, Double salary,String department) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.department=department;
+    }
+    public Employee(){}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
